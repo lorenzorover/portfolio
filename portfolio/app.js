f31b7db993a41299a28a1e5d9fa0d6b5489d8c47
LorenzoRover <lorenzo.rover66@gmail.com>
2024-08-29 06:49:51 -0300

    funcao botao sublinhado

--- portfolio/app.js
+++ portfolio/app.js
@@ -1,6 +1,7 @@
-esconderParagrafos();
+paginaInicio();
 
 let valorAnterior;
+let botaoAnterior;
 
 function esconderParagrafos() {
     // Desativar os parágrafos das informações sobre mim da página
@@ -10,16 +11,31 @@ function esconderParagrafos() {
     });
 }
 
+function paginaInicio() {
+    esconderParagrafos();
+    document.getElementById('paragrafo-1');
+    // caso o tema escuro nao funcione normalmente, colocar ele aqui
+}
+
 function mostrarParagrafo(valor) {
     const valorAtual = document.getElementById('paragrafo-' + valor);
+    const botaoAtual = document.getElementById('button-info-' + valor);
+    // colocar o css do botao selecionado aqui também
 
     if (valor > 0 && valor < 7) {
         valorAtual.style.display = 'block'; // ou flex
+        botaoAtual.classList.add('nome do css aqui');
 
         if (valorAnterior !== undefined) {
             valorAnterior.style.display = 'none';
+            
+        }
+
+        if (botaoAnterior !== undefined) {
+            botaoAnterior.classList.remove('');
         }
     
+        botaoAnterior = botaoAtual;
         valorAnterior = valorAtual;
     }
 }
