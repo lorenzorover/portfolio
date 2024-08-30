ab2c72f30cc3784587e790b8e9ca42ee35d49ca4
LorenzoRover <lorenzo.rover66@gmail.com>
2024-08-30 02:11:57 -0300

    erro dos botoes

--- portfolio/app.js
+++ portfolio/app.js
@@ -1,7 +1,8 @@
-paginaInicio();
+let valorAnterior = document.getElementById('paragrafo-1');
+let botaoAnterior = document.getElementById('button-info-1');
+const checkBox = document.getElementById('trocar-tema');
 
-let valorAnterior;
-let botaoAnterior;
+paginaInicio();
 
 function esconderParagrafos() {
     // Desativar os parágrafos das informações sobre mim da página
@@ -13,34 +14,34 @@ function esconderParagrafos() {
 
 function paginaInicio() {
     esconderParagrafos();
-    document.getElementById('paragrafo-1');
+    valorAnterior.style.display = 'block';
+    botaoAnterior.classList.add('button-info-selecionado');
+
     // caso o tema escuro nao funcione normalmente, colocar ele aqui
 }
 
 function mostrarParagrafo(valor) {
     const valorAtual = document.getElementById('paragrafo-' + valor);
     const botaoAtual = document.getElementById('button-info-' + valor);
-    // colocar o css do botao selecionado aqui também
-
-    if (valor > 0 && valor < 7) {
-        valorAtual.style.display = 'block'; // ou flex
-        botaoAtual.classList.add('nome do css aqui');
-
-        if (valorAnterior !== undefined) {
-            valorAnterior.style.display = 'none';
-            
-        }
-
-        if (botaoAnterior !== undefined) {
-            botaoAnterior.classList.remove('');
-        }
     
-        botaoAnterior = botaoAtual;
-        valorAnterior = valorAtual;
+    if (valorAnterior !== valorAtual) {
+        if (valor > 0 && valor < 7) {
+            valorAtual.style.display = 'block'; // ou flex
+            botaoAtual.classList.add('button-info-selecionado');
+    
+            if (valorAnterior !== undefined) {
+                valorAnterior.style.display = 'none';
+            }
+    
+            if (botaoAnterior !== undefined) {
+                botaoAnterior.classList.remove('button-info-selecionado');
+            }
+        
+            botaoAnterior = botaoAtual;
+            valorAnterior = valorAtual;
+        }
     }
 }
-
-const checkBox = document.getElementById('trocar-tema');
 
 checkBox.addEventListener('change', function() {
 
